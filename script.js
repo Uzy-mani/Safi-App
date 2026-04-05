0961b0fc1a9eb2e59973913c8a896700ad53c118
Uzy-mani <otwouzy@gmail.com>
2026-04-05 17:30:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+document.getElementById("signupForm")?.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  alert("Account created successfully!");
+
+  window.location.href = "dashboard.html";
+});
+
+const jobs = [
+  "Plumber needed in Lagos",
+  "Tailor needed for wedding dress",
+  "Graphic designer for logo",
+  "Electrician needed urgently"
+];
+
+const jobList = document.getElementById("jobList");
+
+if (jobList) {
+  jobs.forEach(job => {
+    const li = document.createElement("li");
+    li.textContent = job;
+    jobList.appendChild(li);
+  });
+}
+
 const profileForm = document.getElementById("profileForm");
 
 if (profileForm) {
